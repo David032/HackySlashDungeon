5511f1643778b49b27822b53fcb0496f324ded31
Unknown <davek032@outlook.com>
2019-03-11 21:12:56 +0000

    Basic Demo
    Few enemies to boop, few weapons to play around with and a ball to throw(but no basketball hoop!)

---
+++ HackySlashDungeon/Assets/Scripts/BasicCombat.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicCombat : MonoBehaviour {
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Weapon")
+        {
+            this.gameObject.SetActive(false);
+        }
+    }
+}

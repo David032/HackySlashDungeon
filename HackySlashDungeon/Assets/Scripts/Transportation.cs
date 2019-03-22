4827ca8defc5a682556d39b4e69e23e3db464c53
Unknown <davek032@outlook.com>
2019-03-22 17:11:24 +0000

    Structure for a point to point teleportation system is now in place, just need to map all of the points. However, it currently works of wasd as the documentation of getting the remote input is rather sparse

---
+++ HackySlashDungeon/Assets/Scripts/Transportation.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Oculus;
+
+public class Transportation : MonoBehaviour {
+
+    public GameObject[] Points;
+
+    public int currentPoint = 0;
+
+    private void Start()
+    {        
+        transform.position = Points[0].transform.position;
+    }
+
+    private void Update()
+    {
+        moveController();
+    }
+
+    void moveController()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            if (currentPoint == 0)
+            {
+                move(1);
+            }
+            if (currentPoint == 1)
+            {
+                print("Not yet");
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            if (currentPoint == 2)
+            {
+                move(3);
+            }
+            if (currentPoint == 4)
+            {
+                move(2);
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            if (currentPoint == 1)
+            {
+                move(0);
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            if (currentPoint == 2)
+            {
+                move(4);
+            }
+            if (currentPoint == 3)
+            {
+                move(2);
+            }
+        }
+    }
+
+    void move(int targetPoint)
+    {
+        currentPoint = targetPoint;
+        transform.position = Points[targetPoint].transform.position;
+    }
+}

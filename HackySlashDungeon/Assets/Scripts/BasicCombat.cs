b0c4c50951553965ad4d7946897ccba4962dd198
David032 <davek032@outlook.com>
2019-03-27 15:17:57 +0000

    Combat...polish?
    Enemy moves, and when you hit it with a weapon, it dies.

--- HackySlashDungeon/Assets/Scripts/BasicCombat.cs
+++ HackySlashDungeon/Assets/Scripts/BasicCombat.cs
@@ -4,11 +4,23 @@ using UnityEngine;
 
 public class BasicCombat : MonoBehaviour {
 
+    public AudioSource deathSound;
+    public int health = 3;
+
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Weapon")
         {
-            this.gameObject.SetActive(false);
+            if (health == 0)
+            {
+                deathSound.Play();
+                this.gameObject.SetActive(false);
+            }
+            else
+            {
+                health -= 1;
+            }
+
         }
     }
 }
